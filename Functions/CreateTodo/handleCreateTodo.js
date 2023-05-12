2a1b5b61490b3bc5bb98d32925b2f01e01b23c2b
Tony5300 <tony_tolen@ziggo.nl>
2023-05-12 17:40:52 +0200

    feat: created the delete btn and items are added to the array

--- Functions/CreateTodo/handleCreateTodo.js
+++ Functions/CreateTodo/handleCreateTodo.js
@@ -1,13 +1,29 @@
 import { v4 as uuid } from "uuid";
-const todoId = uuid();
-console.log(`todoId:`, todoId);
 
 const handleCreateTodo = (taskArray, todoInputValue) => {
-  console.log(`hello world from the createTodo function`, taskArray);
+  // target the parent of the li (ul)
   const todoListContainer = document.getElementById("todo_list_container");
+  // create li
   const createTodoItem = document.createElement(`li`);
+  // create a deleteBtn
+  const createDeleteBtn = document.createElement(`button`);
+  // set the text for the created items
   createTodoItem.textContent = `${todoInputValue}`;
+  createDeleteBtn.textContent = "delete";
+  // add the li to the ul
   todoListContainer.appendChild(createTodoItem);
+  createTodoItem.appendChild(createDeleteBtn);
+
+  const newTodo = {
+    id: uuid(),
+    text: todoInputValue,
+  };
+
+  // create a new array with the existing taskArray and the newTodo object
+  const newTaskArray = [...taskArray, newTodo];
+
+  console.log(`taskArray`, newTaskArray);
+  return newTaskArray;
 };
 
 export default handleCreateTodo;
--- main.js
+++ main.js
@@ -1,25 +1,33 @@
 import "./style.css";
 import handleCreateTodo from "./Functions/CreateTodo/handleCreateTodo";
-import handleShowTasks from "./Functions/Show/handleShowTasks";
 
-const addTodoBtn = document.getElementById("add_todo_btn"); // target the add todo btn
-const todoInput = document.getElementById("todo_input_field"); // target the input
+// get the addTodoBtn
+const addTodoBtn = document.getElementById("add_todo_btn");
+// get the todoInput
+const todoInput = document.getElementById("todo_input_field");
+// get the form
 const resetTodoInput = document.getElementById("todo_form"); // target the form
-
+// default value of the input
 let todoInputValue = "";
 
-const taskArray = []; // array where we store the data to
+// array where we will store the todo's in
+let taskArray = [];
 
+//! function for handling the input field
 const handleInput = (e) => {
+  // assign the value to the variable
   todoInputValue = e.target.value;
 };
 todoInput.addEventListener(`input`, handleInput);
 
+//! master function that take care of multiple things when you click the add todo btn
 const handleAdd = (e) => {
-  handleCreateTodo(taskArray, todoInputValue);
-  e.preventDefault(); // prevent the form from refreshing the browser everytime we add a new todo
-  resetTodoInput.reset(); // reset the form to a empty string after adding a todo
+  // function for adding todo's
+  taskArray = handleCreateTodo(taskArray, todoInputValue); // assign the returned value of handleCreateTodo to the taskArray variable and than call the handleShowTasks to show the updated list to the DOM
+
+  // prevent the form from refreshing the browser everytime we add a new todo
+  e.preventDefault();
+  // reset the form to a empty string after adding a todo
+  resetTodoInput.reset();
 };
 addTodoBtn.addEventListener(`click`, handleAdd);
-
-handleShowTasks(taskArray); // show the tasks to the DOM
